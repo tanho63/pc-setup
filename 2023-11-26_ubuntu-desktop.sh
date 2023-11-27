d62f56673c52ff5e34700e1bf15e5fa9fb0dbe59
Tan Ho <38083823+tanho63@users.noreply.github.com>
2023-11-27 09:46:42 -0500

    Update 2023-11-26_ubuntu-desktop.sh

--- 2023-11-26_ubuntu-desktop.sh
+++ 2023-11-26_ubuntu-desktop.sh
@@ -8,36 +8,40 @@
 # 
 # I realize this is kind of like what Nix does. I might explore that in a future iteration. 
 
-
 # Start by updating the existing packages
 apt-get update -qq && apt-get upgrade -qq
 
 # Using snap to install all the regular stuff
-sudo snap install slack discord spotify vlc obs-studio gimp inkscape firefox chromium steam 
+sudo snap install slack discord spotify vlc obs-studio gimp inkscape firefox chromium steam blue-recorder
 
-# Shamelessly installing R from rocker scripts because I can
-R_HOME=/user/local/lib/R
-TZ=Etc/UTC
-CRAN=https://p3m.dev/cran/__linux__/jammy/latest
-LANG=en_US.UTF-8
 TAN_HOME=/home/tan
-
 set -euxo pipefail
 
-curl -fsSL https://github.com/rocker-org/rocker-versioned2/raw/master/scripts/install_R_source.sh | bash
-curl -fsSL https://github.com/rocker-org/rocker-versioned2/raw/master/scripts/setup_R.sh | bash
+## Install R
+# unhold packages in case any packages are pinned/on-hold 
+# (mostly in case I have to rerun this several times and the later hold causes problems)
+apt-mark unhold r-base r-base-.
+# Instructions mostly from https://cran.rstudio.com/bin/linux/ubuntu/
+apt update -qq && apt install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt install --no-install-recommends r-base-dev
+# pin apt versions of R stuff so that it doesn't autoupdate
+apt-mark hold r-base r-base-. r-recommended
 
-apt-get install gdebi-core
-wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.09.1-494-amd64.deb
-gdebi rstudio-2023.09.1-494-amd64.deb
+# haven't figured out automatically installing latest RStudio version yet
+apt-get install -qq gdebi-core
+wget -O $TAN_HOME/rstudio.deb https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.09.1-494-amd64.deb
+gdebi $TAN_HOME/rstudio.deb
 
 # Tailscale has been awesome for homelab/vpn
+apt-get install -qq libcurl4 curl
 curl -fsSL https://tailscale.com/install.sh | bash
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> $TAN_HOME/.bashrc
-wget -0 $TAN_HOME/.config/starship.toml https://github.com/tanho63/pc-setup/raw/main/starship.toml
+wget -O $TAN_HOME/.config/starship.toml https://github.com/tanho63/pc-setup/raw/main/starship.toml
 
 # Install logiops for mouse stuff
 sudo apt-get install -qq build-essential cmake pkg-config libevdev-dev libudev-dev libconfig++-dev libglib2.0-dev
@@ -50,6 +54,12 @@ make
 make install
 cd $TAN_HOME
 wget -O /etc/logid.cfg https://github.com/tanho63/pc-setup/raw/main/logiops.cfg
+
+# TODO Investigate if solaar is sufficient/better than logiops?
+# Needed it for disabling hi-res mouse scrolling
+add-apt-repository ppa:solaar-unifying/stable
+apt update -qq
+apt install -qq solaar
 
 # disable ubuntu advertising
 wget https://github.com/Skyedra/UnspamifyUbuntu/raw/master/fake-ubuntu-advantage-tools/fake-ubuntu-advantage-tools.deb
